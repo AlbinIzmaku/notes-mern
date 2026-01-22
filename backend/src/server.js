8220c70677bea8fd20bc09b7e3a2a70b1b72dceb
Albin Izmaku <albinizmaku2@gmail.com>
2026-01-22 23:16:38 +0100

    Added NoteCard component

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,16 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
+
 // app.use((req, res, next) => {
 //   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //   next();
