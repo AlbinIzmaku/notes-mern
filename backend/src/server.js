65707ab0f6d014232f4d025ac9a13209cbfd47ce
Albin Izmaku <albinizmaku2@gmail.com>
2026-01-26 20:08:43 +0100

    Cleaned server

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,11 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//   next();
-// });
-
 app.use("/api/notes", notesRoutes);
 
 connectDB().then(() => {
